b4141eb4737cee86fa31a978a1ce108fea10a204
ashif-iqbal <contact2ashif@gmail.com>
2026-03-09 15:22:19 +0530

    uc3

---
+++ src/palindromeUc3.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class palindromeUc3 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Palindrome Checker Application");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String word = scanner.nextLine();
+
+        String reversed = "";
+
+        for(int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        if(word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+
+        scanner.close();
+    }
+}
